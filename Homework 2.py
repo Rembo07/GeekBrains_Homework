af985f68b0e2678e40abffa07705e77238469b91
Artem <fffoig2@gmail.com>
2022-03-05 18:30:28 +0700

    added upgrades

--- Homework 2.py
+++ Homework 2.py
@@ -1,9 +1,17 @@
-numbers = []
-number_1 = []
-coob = None
+def sum_digits(value):
+    res = 0
 
-for numb in range(1, 1000):
-    if numb % 2 != 0:
-        coob = numb ** 3
-        numbers.append(coob)  
-print(numbers)
+    while value != 0:
+        res += value % 10
+        value //= 10
+
+    return res
+
+
+arr = [i**3 for i in range(1, 1001, 2)]
+
+res1 = sum(filter(lambda num: sum_digits(num) % 7 == 0, arr))
+res2 = sum(filter(lambda num: sum_digits(num + 17) % 7 == 0, arr))
+
+print(res1)
+print(res2)
